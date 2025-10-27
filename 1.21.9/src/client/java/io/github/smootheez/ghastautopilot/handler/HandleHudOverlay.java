7723193f8c04e684663c7d96dcd7a2b35c48b3cb
Smootheez <raehanstars@gmail.com>
2025-10-27 15:21:16 +0700

    Add icon position config, new options, and update changelog.

--- 1.21.9/src/client/java/io/github/smootheez/ghastautopilot/handler/HandleHudOverlay.java
+++ 1.21.9/src/client/java/io/github/smootheez/ghastautopilot/handler/HandleHudOverlay.java
@@ -1,6 +1,7 @@
 package io.github.smootheez.ghastautopilot.handler;
 
 import io.github.smootheez.ghastautopilot.config.*;
+import io.github.smootheez.ghastautopilot.config.option.*;
 import io.github.smootheez.ghastautopilot.util.*;
 import io.github.smootheez.smoothiezapi.config.*;
 import net.fabricmc.api.*;
@@ -9,19 +10,69 @@ import net.minecraft.client.*;
 import net.minecraft.client.gui.*;
 import net.minecraft.client.renderer.*;
 import net.minecraft.resources.*;
+import org.jetbrains.annotations.*;
 
 @Environment(EnvType.CLIENT)
 public class HandleHudOverlay implements HudElement {
     private static final ResourceLocation AUTOPILOT_ACTIVE_ICON =
             ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "textures/gui/autopilot_active.png");
     private static final GhastAutopilotConfig CONFIG = ConfigManager.getConfig(GhastAutopilotConfig.class);
+    private static final int ICON_SIZE = 16;
 
     @Override
     public void render(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
         if (Boolean.FALSE.equals(CONFIG.getAutopilotIcon().getValue()) || !GhastAutopilotUtil.isAutoPilot()) return;
-        var iconSize = 16;
 
-        guiGraphics.blit(
-                RenderPipelines.GUI_TEXTURED, AUTOPILOT_ACTIVE_ICON, 5, 5, 0, 0, iconSize, iconSize, iconSize, iconSize);
+        var iconPosition = setPosition(CONFIG.getIconPosition().getValue(), guiGraphics);
+
+        guiGraphics.blit(RenderPipelines.GUI_TEXTURED, AUTOPILOT_ACTIVE_ICON,
+                iconPosition.x(), iconPosition.y(), 0, 0, ICON_SIZE, ICON_SIZE, ICON_SIZE, ICON_SIZE);
     }
+
+    private static @NotNull ArangeIconPosition setPosition(IconPosition value, GuiGraphics guiGraphics) {
+        int width = guiGraphics.guiWidth();
+        int height = guiGraphics.guiHeight();
+
+        // Small margin so the icon isn’t glued to the edges
+        final int margin = 3;
+        final int size = ICON_SIZE;
+
+        int x = 0;
+        int y = 0;
+
+        switch (value) {
+            case TOP_LEFT -> {
+                x = margin;
+                y = margin;
+            }
+            case TOP_MIDDLE -> {
+                x = (width - size) / 2;
+                y = margin;
+            }
+            case TOP_RIGHT -> {
+                x = width - size - margin;
+                y = margin;
+            }
+            case RIGHT_MIDDLE -> {
+                x = width - size - margin;
+                y = (height - size) / 2;
+            }
+            case BOTTOM_RIGHT -> {
+                x = width - size - margin;
+                y = height - size - margin;
+            }
+            case BOTTOM_LEFT -> {
+                x = margin;
+                y = height - size - margin;
+            }
+            case LEFT_MIDDLE -> {
+                x = margin;
+                y = (height - size) / 2;
+            }
+        }
+
+        return new ArangeIconPosition(x, y);
+    }
+
+    record ArangeIconPosition(int x, int y) {}
 }
